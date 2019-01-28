ce4ff68d5c216856f271ddf1a2c5a4348bfbbcda
gonzalo <gonzalo_lledo@outlook.com>
2019-01-28 11:30:11 +0100

    Jin chen dictador

--- app/src/main/java/com/example/gonza/appfirebase/MainActivity.java
+++ app/src/main/java/com/example/gonza/appfirebase/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.gonza.appfirebase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    String test1;
+    EditText etUsuario;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUsuario = findViewById(R.id.etUsuario);
+
+    }
+
+    public void acceder(View v){
+
+        String usuario = etUsuario.getText().toString();
+
+        if (usuario.trim().equals("")){
+            Toast.makeText(getBaseContext(),getString(R.string.toast_bienvenida),Toast.LENGTH_LONG).show();
+        }else{
+            Intent i = new Intent(this, VentanaBotones.class);
+            i.putExtra(getString(R.string.clave_nombre), usuario);
+            startActivity(i);
+        }
     }
 }
--- app/src/main/java/com/example/gonza/appfirebase/VentanaBotones.java
+++ app/src/main/java/com/example/gonza/appfirebase/VentanaBotones.java
@@ -2,12 +2,21 @@ package com.example.gonza.appfirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class VentanaBotones extends AppCompatActivity {
+
+    TextView tvTexto;
+    String nombre;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ventana_botones);
+
+        tvTexto = findViewById(R.id.txtExplicacion);
+        nombre = getIntent().getStringExtra(getString(R.string.clave_nombre));
+        tvTexto.setText(String.format(getString(R.string.txt_explicacion),nombre));
     }
 }
