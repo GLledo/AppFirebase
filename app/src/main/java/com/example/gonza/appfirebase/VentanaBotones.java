be9a55077ddda3d66c5fdf0c5dc33ed327f08a30
jorge <21746516@live.uem.es>
2019-01-28 11:19:16 +0100

    AñadidoBotones

--- app/src/main/java/com/example/gonza/appfirebase/VentanaBotones.java
+++ app/src/main/java/com/example/gonza/appfirebase/VentanaBotones.java
@@ -2,6 +2,7 @@ package com.example.gonza.appfirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class VentanaBotones extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class VentanaBotones extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ventana_botones);
+    }
+
+
+    public void InsertarViaje(View view) {
+    }
+
+    public void ModificarViaje(View view) {
+    }
+
+    public void ConsultarTodosLosViajes(View view) {
+    }
+
+    public void ConsultarViajes5Dias(View view) {
+    }
+
+    public void salir(View view) {
     }
 }
