dd77fb4765346a2aee1292b9fa51bf67a0f2941f
jorge <21746516@live.uem.es>
2019-01-28 12:02:01 +0100

    JavaBeansse

--- app/src/main/java/com/example/gonza/appfirebase/javabeans/MensajeAdaptador.java
+++ app/src/main/java/com/example/gonza/appfirebase/javabeans/MensajeAdaptador.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.prueba.primerfirebase.R;
+import com.example.gonza.appfirebase.R;
 
 import java.util.ArrayList;
 
