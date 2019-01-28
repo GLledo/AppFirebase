f442c63d4cbe4624a7fba590e511518d65e60e5e
gonzalo <gonzalo_lledo@outlook.com>
2019-01-28 13:51:53 +0100

    viajessss

--- app/src/main/java/com/example/gonza/appfirebase/InsertarViaje.java
+++ app/src/main/java/com/example/gonza/appfirebase/InsertarViaje.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.example.gonza.appfirebase.javabeans.Viaje;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class InsertarViaje extends AppCompatActivity {
 
@@ -15,6 +17,8 @@ public class InsertarViaje extends AppCompatActivity {
     EditText etPrecio;
     EditText etDias;
     String nombre;
+
+    private DatabaseReference dbR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +32,15 @@ public class InsertarViaje extends AppCompatActivity {
         etDias = findViewById(R.id.etDuracion);
 
         nombre = ((MyApp)getApplicationContext()).getNombre();
+
+        dbR = FirebaseDatabase.getInstance().getReference().child("viaje");
+
     }
 
     public void crearViaje(View v){
 
-       Viaje msj = new Viaje(etNombre.getText().toString(),etPrecio.getText().toString(),
-               etDias.getText().toString(),etCiudad.getText().toString(),etPais.getText().toString());
+       Viaje msj = new Viaje(etNombre.getText().toString(),Double.parseDouble(etPrecio.getText().toString()),
+               Integer.parseInt(etDias.getText().toString()),etCiudad.getText().toString(),etPais.getText().toString(),nombre);
         String clave = dbR.push().getKey();
         dbR.child(clave).setValue(msj);
 
