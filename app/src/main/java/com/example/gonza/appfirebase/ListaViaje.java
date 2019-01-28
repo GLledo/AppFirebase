54b1ec41a0ccf8af1a6360b6248ddc658fd22203
gonzalo <gonzalo_lledo@outlook.com>
2019-01-28 13:05:09 +0100

    merge

---
+++ app/src/main/java/com/example/gonza/appfirebase/ListaViaje.java
@@ -0,0 +1,13 @@
+package com.example.gonza.appfirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaViaje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_viaje);
+    }
+}
